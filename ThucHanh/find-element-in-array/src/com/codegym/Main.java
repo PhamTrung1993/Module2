e6074c4f4feba4dfc68fa45b96c37448dd70277a
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-08-05 10:32:05 +0700

    find element in array

---
+++ ThucHanh/find-element-in-array/src/com/codegym/Main.java
@@ -0,0 +1,27 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] student = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name's student: ");
+        String input_name = scanner.nextLine();
+
+        boolean isExist = false;
+        for (int i = 0; i < student.length; i++) {
+            // Lệnh equal để so sánh chuỗi.
+            if (student[i].equals(input_name)) {
+                System.out.println("Poisition of the students in the list " + input_name +" is: " + i);
+                isExist =  true;
+                break;
+            }
+
+        }
+        if (!isExist) {
+            System.out.println("Not found" + input_name + " in the list");
+        }
+    }
+}
