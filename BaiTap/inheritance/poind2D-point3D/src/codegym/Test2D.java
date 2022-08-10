6d90748a46dc770ffa41afb253d84ca5d929ef85
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-08-10 13:55:33 +0700

    point 2D-3D

---
+++ BaiTap/inheritance/poind2D-point3D/src/codegym/Test2D.java
@@ -0,0 +1,11 @@
+package codegym;
+
+public class Test2D {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D();
+        System.out.println(point2D);
+
+        point2D = new Point2D(5,7);
+        System.out.println(point2D);
+    }
+}
---
+++ BaiTap/inheritance/poind2D-point3D/src/codegym/Test3D.java
@@ -0,0 +1,11 @@
+package codegym;
+
+public class Test3D {
+    public static void main(String[] args) {
+        Point3D point3D = new Point3D();
+        System.out.println(point3D);
+
+        point3D = new Point3D(3,5,6);
+        System.out.println(point3D);
+    }
+}
