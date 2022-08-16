938e72d82a26241bf076e0301d6d54a7b562e628
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-08-16 10:08:44 +0700

    fix and test case

--- BaiTap/AutomatedTDD/nextday-calculator/src/codegym/NextDayCalculator.java
+++ BaiTap/AutomatedTDD/nextday-calculator/src/codegym/NextDayCalculator.java
@@ -8,7 +8,7 @@ public class NextDayCalculator {
         return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
     }
 
-    public static int numberOfDaysInTheMonth(int month,int year) {
+    public static int lastDaysInTheMonth(int month,int year) {
         switch (month) {
             case 1:
             case 3:
@@ -36,17 +36,20 @@ public class NextDayCalculator {
         }
     }
 
-    public static void theNextDay(int year, int month, int day) {
+    public static String getNextDay(int year, int month, int day) {
         int nextyear = year;
         int nextmonth = month;
         int nextday = day;
+        String concatenation = "/";
 
-        if (year > 0 && month < 13 && day> 0 && day < numberOfDaysInTheMonth(month,year)) {
+        if (year > 0 && month < 13 && day > 0 && day <= lastDaysInTheMonth(month,year)) {
             nextday = day + 1;
-            if (month != 12 && day == numberOfDaysInTheMonth(month,year)) {
+            if (month == 7 && day == 31) {
                 nextday = 1;
-                nextmonth = month +1;
-            } else if (month == 12 && day == numberOfDaysInTheMonth(month, year)) {
+                nextmonth = month + 1;
+
+            }
+            else if (month == 12 && day == lastDaysInTheMonth(month, year)) {
                 nextday = 1;
                 nextyear = nextyear +1;
                 nextmonth = 1;
@@ -68,7 +71,7 @@ public class NextDayCalculator {
             }
         }
 
-        System.out.println("\n The Next day is: "+ nextday+" "+nextmonth+ " "+nextyear);
+        return "The Next day is: "+ nextday+concatenation+nextmonth+concatenation+nextyear;
 
     }
 
@@ -84,6 +87,7 @@ public class NextDayCalculator {
            year = scanner.nextInt();
         }
         while (year < 0 || month < 1 || month > 12 || day < 0 || day > 31);
-        theNextDay(year,month,day);
+        ;
+        System.out.println(getNextDay(year,month,day));
     }
 }
--- BaiTap/AutomatedTDD/nextday-calculator/test/codegym/NextDayCalculatorTest.java
+++ BaiTap/AutomatedTDD/nextday-calculator/test/codegym/NextDayCalculatorTest.java
@@ -1,5 +1,6 @@
 package codegym;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.StringTokenizer;
@@ -23,22 +24,42 @@ class NextDayCalculatorTest {
         int year = 2022;
         int expected = 31;
 
-        int result = NextDayCalculator.numberOfDaysInTheMonth(month,year);
+        int result = NextDayCalculator.lastDaysInTheMonth(month,year);
         assertEquals(expected, result);
     }
 
     @Test
-    void testtheNextDay() {
+    @DisplayName("case 30/07/2022")
+    void test30072022() {
             int year = 2022;
             int month = 7;
             int day = 30;
-            String expected = "The Next day is: 31 7 2022";
+            String expected = "The Next day is: 31/7/2022";
 
-            String result = NextDayCalculator.theNextDay(year,month,day);
+            String result = NextDayCalculator.getNextDay(year,month,day);
+        assertEquals(expected, result);
+    }
+    @Test
+    @DisplayName("case 01/02/2022")
+    void test01022022() {
+            int year = 2022;
+            int month = 2;
+            int day = 1;
+            String expected = "The Next day is: 2/2/2022";
+
+            String result = NextDayCalculator.getNextDay(year,month,day);
+        assertEquals(expected, result);
+    }
+    @Test
+    @DisplayName("case 28/02/2022")
+    void test28022022() {
+        int year = 2022;
+        int month = 2;
+        int day = 28;
+        String expected = "The Next day is: 1/3/2022";
+
+        String result = NextDayCalculator.getNextDay(year,month,day);
         assertEquals(expected, result);
     }
 
-    @Test
-    void main() {
-    }
 }
