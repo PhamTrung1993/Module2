fc7c461ba87d9cbabddca5e19b8cdf117b04f122
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-08-19 15:04:55 +0700

    done

--- ThucHanh/treemap/sorting-with-comparable-and-comparator/src/codegym/Main.java
+++ ThucHanh/treemap/sorting-with-comparable-and-comparator/src/codegym/Main.java
@@ -1,2 +1,33 @@
-package codegym;public class Main {
+package codegym;
+
+import java.util.ArrayList;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Kien",30,"HT");
+        Student studentOne = new Student("Nam",26,"HN");
+        Student studentTwo = new Student("Anh",38,"HN");
+        Student studentThree = new Student("Tung",38,"HN");
+
+        List<Student> lists = new ArrayList<Student>();
+        lists.add(student);
+        lists.add(studentOne);
+        lists.add(studentTwo);
+        lists.add(studentThree);
+
+        Collections.sort(lists);
+        for (Student st: lists) {
+            System.out.println(st.toString());
+        }
+
+    AgeComparator ageComparator = new AgeComparator();
+    Collections.sort(lists,ageComparator);
+    System.out.println("So sanh theo tuoi: ");
+    for (Student st: lists) {
+        System.out.println(st.toString());
+    }
+    }
 }
