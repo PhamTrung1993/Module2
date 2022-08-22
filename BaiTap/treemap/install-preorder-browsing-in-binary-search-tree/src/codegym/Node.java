aa608dc33a25138886b7d18449968a1b2ea4e27c
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-08-22 22:52:30 +0700

    preorder

---
+++ BaiTap/treemap/install-preorder-browsing-in-binary-search-tree/src/codegym/Node.java
@@ -0,0 +1,13 @@
+package codegym;
+
+public class Node {
+    protected int element;
+    protected Node left;
+    protected Node right;
+
+    public Node(int e) {
+        this.element =e;
+        this.left = null;
+        this.right = null;
+    }
+}
